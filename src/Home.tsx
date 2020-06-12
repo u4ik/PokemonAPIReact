8331cb4673327afa16e123d90165eb279218e211
u4ik <affirmativeamit@gmail.com>
2020-06-11 23:58:41 -0400

    added web pokemon font,div around card,placeholder background

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,20 +1,30 @@
 import React, {useState} from 'react'
 import PokeCard from './assets/pokemoncard.png'
 import PokeLogo from './assets/pokemonlogo.png'
+import PokeBackground from './assets/pokemonbackground.jpg'
 
 
 
 const Home = () => {
 
+    const gottaCatchEmAllStyle ={
+        fontSize: '2vw',
+        color:'white'
+    }
+
+    
+    
 
 
 return(
     <div>
         <img alt="Pokemon Logo" src= {PokeLogo}/>
-        <p>Gotta fetch 'em all!</p>
-    <div>
-        <img alt='Pokemon Card' src ={PokeCard}/>
-    </div>
+        <p style={gottaCatchEmAllStyle}>Gotta fetch 'em all!</p>
+        <div style={{ display:'flex', flexDirection:'row', justifyContent:'center'}}>
+            <div style={{backgroundColor:'white', width:'fit-content',height:'585px', borderRadius:'30px'}}>
+                <img alt='Pokemon Card' src ={PokeCard}/>
+            </div>
+        </div>
     
     </div>
 )
