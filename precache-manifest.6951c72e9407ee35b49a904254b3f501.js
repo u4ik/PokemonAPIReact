8da810568cbdce1f958cab6d91db7918d2786f96
u4ik <affirmativeamit@gmail.com>
2020-06-15 23:33:33 -0400

    Updates

---
+++ precache-manifest.6951c72e9407ee35b49a904254b3f501.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a796b27a9a60fd988a7a692f3147c6",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ff1411df3001b722971f",
+    "url": "/react-gh-pages/static/css/main.319bb433.chunk.css"
+  },
+  {
+    "revision": "34476df442c5973271f2",
+    "url": "/react-gh-pages/static/js/2.c4e0370a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-gh-pages/static/js/2.c4e0370a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1411df3001b722971f",
+    "url": "/react-gh-pages/static/js/main.aa428f79.chunk.js"
+  },
+  {
+    "revision": "aeb6ad679e40744754c2",
+    "url": "/react-gh-pages/static/js/runtime-main.99d0e939.js"
+  },
+  {
+    "revision": "775c3814e9c1f228d495333e07580d59",
+    "url": "/react-gh-pages/static/media/Andy-Bold.775c3814.ttf"
+  },
+  {
+    "revision": "f1fd74abdde20240ce7a6c0aa0a02dd3",
+    "url": "/react-gh-pages/static/media/pokemonbackground.f1fd74ab.gif"
+  },
+  {
+    "revision": "9383bf2decb7f23c15f99cc14b4bb24a",
+    "url": "/react-gh-pages/static/media/pokemoncardbug.9383bf2d.png"
+  },
+  {
+    "revision": "09caa0146388df875d38a72cecd64a4b",
+    "url": "/react-gh-pages/static/media/pokemoncarddark.09caa014.png"
+  },
+  {
+    "revision": "b3f3386ef873c7bf73770fefc73716c9",
+    "url": "/react-gh-pages/static/media/pokemoncarddragon.b3f3386e.png"
+  },
+  {
+    "revision": "05d2eb6154ac8c63b4dbd26ad67f997a",
+    "url": "/react-gh-pages/static/media/pokemoncardelectric.05d2eb61.png"
+  },
+  {
+    "revision": "b547ad69c6294b9a0e8dc41fe820552b",
+    "url": "/react-gh-pages/static/media/pokemoncardfairy.b547ad69.png"
+  },
+  {
+    "revision": "2e4bcd4384114445dbbfcd70db14b69b",
+    "url": "/react-gh-pages/static/media/pokemoncardfighting.2e4bcd43.png"
+  },
+  {
+    "revision": "535f7a1ebe4a795165eb43156306fdcd",
+    "url": "/react-gh-pages/static/media/pokemoncardfire.535f7a1e.png"
+  },
+  {
+    "revision": "c58fa0762b629d28c6000d32ef1ef19d",
+    "url": "/react-gh-pages/static/media/pokemoncardflying.c58fa076.png"
+  },
+  {
+    "revision": "54e451b0c6528caac78a98911fe6602d",
+    "url": "/react-gh-pages/static/media/pokemoncardghost.54e451b0.png"
+  },
+  {
+    "revision": "a2c97fa5e3e5d21592848dc6ae44777d",
+    "url": "/react-gh-pages/static/media/pokemoncardgrass.a2c97fa5.png"
+  },
+  {
+    "revision": "d6d8b6d29da7dd6b4004b3c01a621916",
+    "url": "/react-gh-pages/static/media/pokemoncardground.d6d8b6d2.png"
+  },
+  {
+    "revision": "63e11305505514739792492add82be27",
+    "url": "/react-gh-pages/static/media/pokemoncardice.63e11305.png"
+  },
+  {
+    "revision": "09f10bfdf6b02764b96b326a20477554",
+    "url": "/react-gh-pages/static/media/pokemoncardnormal.09f10bfd.png"
+  },
+  {
+    "revision": "51e93a0c30c667693f01a4740b958caf",
+    "url": "/react-gh-pages/static/media/pokemoncardpoison.51e93a0c.png"
+  },
+  {
+    "revision": "5326d1d15d047a309ea8cfe0dbe53ecd",
+    "url": "/react-gh-pages/static/media/pokemoncardpsychic.5326d1d1.png"
+  },
+  {
+    "revision": "7bfb5ef4c4ccff87f0b1ff5783a809ea",
+    "url": "/react-gh-pages/static/media/pokemoncardrock.7bfb5ef4.png"
+  },
+  {
+    "revision": "6b20cb993a21cb55bde8bdb2207a4d06",
+    "url": "/react-gh-pages/static/media/pokemoncardsteel.6b20cb99.png"
+  },
+  {
+    "revision": "cd8bd36c51e87748f1e87ef14b1517ae",
+    "url": "/react-gh-pages/static/media/pokemoncardwater.cd8bd36c.png"
+  },
+  {
+    "revision": "35d27bad187f185073f3ecbf13e614ec",
+    "url": "/react-gh-pages/static/media/pokemonlogo.35d27bad.png"
+  },
+  {
+    "revision": "e522cbecc178d11e89c97c725c54e2f5",
+    "url": "/react-gh-pages/static/media/pokemontheme.e522cbec.mp3"
+  },
+  {
+    "revision": "c8529c16937ab3f7c0b77177c3441936",
+    "url": "/react-gh-pages/static/media/typebug.c8529c16.png"
+  },
+  {
+    "revision": "143cdc0a978e0ec9b63dba6fbf507826",
+    "url": "/react-gh-pages/static/media/typecolorless.143cdc0a.png"
+  },
+  {
+    "revision": "cf0523dd34aad6412f621cc4a25bdf95",
+    "url": "/react-gh-pages/static/media/typedark.cf0523dd.png"
+  },
+  {
+    "revision": "fd9b84fdd844f45509d42f26910b3307",
+    "url": "/react-gh-pages/static/media/typedragon.fd9b84fd.png"
+  },
+  {
+    "revision": "c3d4994196cd8cc4c1a0f185c02a6b06",
+    "url": "/react-gh-pages/static/media/typeelectric.c3d49941.png"
+  },
+  {
+    "revision": "f2ec9a26cd38462c7befa974f4cac24f",
+    "url": "/react-gh-pages/static/media/typefairy.f2ec9a26.png"
+  },
+  {
+    "revision": "69dee20f260599937429c4f447c3a3e1",
+    "url": "/react-gh-pages/static/media/typefighting.69dee20f.png"
+  },
+  {
+    "revision": "25b8e02c151e5dbf20142c6828025033",
+    "url": "/react-gh-pages/static/media/typefire.25b8e02c.png"
+  },
+  {
+    "revision": "31de7350f3bec65d59d280717d0ac0e9",
+    "url": "/react-gh-pages/static/media/typeflying.31de7350.png"
+  },
+  {
+    "revision": "da6fd439713ed9fdf947a221d546013d",
+    "url": "/react-gh-pages/static/media/typeghost.da6fd439.png"
+  },
+  {
+    "revision": "41f4bcaf89443cff74405e28bb818f8f",
+    "url": "/react-gh-pages/static/media/typegrass.41f4bcaf.png"
+  },
+  {
+    "revision": "33a101dd1a365692a6bbeba6eb3ea664",
+    "url": "/react-gh-pages/static/media/typeground.33a101dd.png"
+  },
+  {
+    "revision": "22d1f20f0b7fd3406da26148fb06abd9",
+    "url": "/react-gh-pages/static/media/typeice.22d1f20f.png"
+  },
+  {
+    "revision": "f01c99e7f245f161d7412ef020e002d5",
+    "url": "/react-gh-pages/static/media/typepoison.f01c99e7.png"
+  },
+  {
+    "revision": "af86495ef5c260c9418cd05f29a9008c",
+    "url": "/react-gh-pages/static/media/typepsychic.af86495e.png"
+  },
+  {
+    "revision": "37cbca74b468ff5b086436c2787f7dde",
+    "url": "/react-gh-pages/static/media/typerock.37cbca74.png"
+  },
+  {
+    "revision": "ad1238b62d0bb9017ac1eb3b41af4427",
+    "url": "/react-gh-pages/static/media/typesteel.ad1238b6.png"
+  },
+  {
+    "revision": "010d87489bf79c9f222361ec42e796dc",
+    "url": "/react-gh-pages/static/media/typewater.010d8748.png"
+  }
+]);
